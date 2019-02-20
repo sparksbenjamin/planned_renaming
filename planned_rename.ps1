65a06ee3d5beb2c5926e8853516c7b1ea9d4584b
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2019-02-19 22:48:07 -0500

    Update planned_rename.ps1

--- planned_rename.ps1
+++ planned_rename.ps1
@@ -1,20 +1,21 @@
 param([String]$src="", [String]$des="", [String]$type="*.*", [String]$rules="")
-
+$_MOVE=1
+if($src -eq ""){exit}
+if($rules -eq ""){exit}
+if($src -eq $des){
+    Write-Output "Move Disabled"
+    $MOVE = 0
+}else{Write-Output "Move Enabled"}
 if(!(Test-Path -Path $des )){
     New-Item -ItemType directory -Path $des
 }
-$_Rules = Import-Csv $rules #| Write-Host "SrcName=" + $_.name | Write-Host "DesName=" + $_.rename | Write-Host "append=" + $_.append 
-
-#Write-Output $_Rules.name
+$_Rules = Import-Csv $rules 
 Get-ChildItem $src -recurse -force -Filter $type | 
 Foreach-Object {
     Foreach ($rule in $_Rules)
     {
-        #$r_n = "?" + $rule.name + "?"
-        $r_n = $rule.name
-        #write-output $r_n
-        #Write-Output $_.FullName
         
+        $r_n = $rule.name        
         if ($_.Name -like "*$r_n*") { 
             Write-Output "Match"
             $rn_rule = $rule.rename + $rule.append
@@ -22,28 +23,24 @@ Foreach-Object {
             $newfilename_tmp = $src + "\" + $newfilename
             $newfilename_full= $des + "\" + $newfilename
             $oldfilename_full = $_.FullName
-            $save = 0
-            #write-output $oldfilename_full
-            #
-            #
-            #
-            #
-            #Write-Output $newfilename_full
             while ($true){
+                $i = 0
                 if(!(Test-Path -Path $newfilename_full )){
                     Rename-Item -Path $oldfilename_full -NewName $newfilename
-                    Move-Item -Path $newfilename_tmp -Destination $newfilename_full
-                    #Rename-Item -Path $oldfilename_full -NewName $newfilename_full
+                    if($_MOVE = 1){Move-Item -Path $newfilename_tmp -Destination $newfilename_full}
                     write-output "Renamed $oldfilename_full to $newfilename_full"
                     break
                 }else{
-                    $newinc = $i++
-                    if ($i > 1){
-                        $newfilename_full = $newfilename_full -replace "$i","$i++"
+                    $newinc = "_" + $i++
+                    $curinc = "_" + $i
+                    if ($curinc > 0){
+                        $newfilename_full = $newfilename_full -replace $curinc,$newinc
                     }
                     else{
+                        $i++
                         $newfilename_full = $newfilename_full + "_" + $i
                     }
+                    $i++
                 }
             }
         }
